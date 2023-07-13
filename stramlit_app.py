fa993b533170be4efcce90312dbf632d1175a27b
Pavan101685 <137398236+Pavan101685@users.noreply.github.com>
2023-07-13 15:24:12 +0530

    Add files via upload

---
+++ stramlit_app.py
@@ -0,0 +1,30 @@
+import streamlit
+import requests
+import pandas
+
+streamlit.title("my parents new healthy diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("omega3 & oat meal")
+streamlit.header("Breakfast Menu")
+streamlit.text("🥣Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔Hard-Boiled Free-Range Egg")
+streamlit.title("🥑 Avocado Toast")
+streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
+
+my_fruit_list = pandas.read_csv(
+    "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
+)
+
+my_fruit_list = my_fruit_list.set_index("Fruit")
+# Let's put a pick list here so they can pick the fruit they want to include
+Fruits_selected = streamlit.multiselect(
+    "Pick some fruits:", list(my_fruit_list.index), ["Avacado", "Strawberries"]
+)
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+# Display the table on the page.
+streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
