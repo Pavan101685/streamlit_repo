42f7c1dff2192d1d47f78f2878019b55664d48be
Pavan101685 <137398236+Pavan101685@users.noreply.github.com>
2023-07-13 18:20:27 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -1,30 +1,33 @@
 import streamlit
-import requests
-import pandas
 import snowflake.connector
+import pandas
+import requests
+from urllib.error import URLError
 
-streamlit.title("my parents new healthy diner")
-streamlit.header("Breakfast Menu")
-streamlit.text("omega3 & oat meal")
-streamlit.header("Breakfast Menu")
-streamlit.text("🥣Omega 3 & Blueberry Oatmeal")
-streamlit.text("🥗Kale, Spinach & Rocket Smoothie")
-streamlit.text("🐔Hard-Boiled Free-Range Egg")
-streamlit.title("🥑 Avocado Toast")
-streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
+ 
 
-my_fruit_list = pandas.read_csv(
-    "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
-)
+streamlit.title('Diner app')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
 
-my_fruit_list = my_fruit_list.set_index("Fruit")
-# Let's put a pick list here so they can pick the fruit they want to include
-Fruits_selected = streamlit.multiselect(
-    "Pick some fruits:", list(my_fruit_list.index), ["Avacado", "Strawberries"]
-)
+ 
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+ 
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'] )
+
+ 
+
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
